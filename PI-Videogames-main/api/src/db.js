2d7a53b176c4dfcbe2e0cb7999ece1f90e2ccb4c
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2024-03-09 13:07:24 -0500

    creacion de rutas

--- PI-Videogames-main/api/src/db.js
+++ PI-Videogames-main/api/src/db.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+
+
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -30,10 +32,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogames, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Videogames.belongsToMany(Genre, { through: 'videogames_genre', as: '' });
+Genre.belongsToMany(Videogames, { through: 'videogames_genre', as: '' });
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
---
+++ PI-Videogames-main/api/src/routes/gamesrouter.js/games.js
@@ -0,0 +1,93 @@
+const { Router } = require('express')
+const router = Router()
+const {
+  getAllGames,
+  getGameById,
+  getGameByName,
+  createGame,
+  editGame,
+  deleteGame,
+} = require('../../controllers/gamesControllers.js')
+
+
+
+router.get('/', async (req, res) => {
+    try {
+      const games = await getAllGames();
+      res.status(200).json(games);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+  });
+
+
+
+  router.get('/name', async (req, res) => {
+    const { name } = req.query;
+  
+    try {
+      if (!name) {
+        return res.status(400).json({ error: 'El parámetro "name" es requerido.' });
+      }
+  
+      const games = await getGameByName(name);
+  
+      if (games.length === 0) {
+        return res.status(404).json({ error: 'No se encontraron videojuegos con el nombre proporcionado.' });
+      }
+  
+      res.status(200).json(games.slice(0, 15)); // Devuelve los primeros 15 juegos
+    } catch (error) {
+      res.status(500).json({ error: 'Error al buscar videojuegos por nombre.' });
+    }
+  });
+
+
+
+
+   router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+      const getById = await getGameById(id)
+      res.status(200).json(getById)
+    } catch (error) {
+      res.status(404).json({ error: error.message })
+    }
+  })
+
+
+  
+router.post('/', async (req, res) => {
+    const newGame = req.body
+    const created = await createGame(newGame)
+    try {
+      res.status(200).json(created)
+    } catch (error) {
+      res.status(404).json({ error: error.message })
+    }
+  })
+
+
+
+  router.put('/:id', async (req, res) => {
+    try {
+      const editedGame = req.body
+      const { id } = req.params
+      const modified = await editGame(editedGame, id)
+      res.status(200).json(modified)
+    } catch (error) {
+      res.status(404).json({ error: error.message })
+    }
+  })
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedGame = await deleteGame(id)
+      res.status(200).json({ message: `El juego "${deletedGame.name}", HA SIDO ELIMINADO` })
+    } catch (error) {
+      res.status(404).json({ error: 'No se pudo eliminar el juego' })
+    }
+  })
+  
+  module.exports = router
